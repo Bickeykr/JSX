ce51800487817b8260e9ac0b22328e673457774e
Bickey Kumar <79741948+Bickeykr@users.noreply.github.com>
2022-09-24 21:34:54 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Create a react app from scratch.
+
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
