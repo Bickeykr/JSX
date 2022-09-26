6a0c75886acabd3e34e4f2f4794cf5f3fb34f121
Bickey Kumar <bickeykumar250@gmail.com>
2022-09-26 05:05:32 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
+ReactDOM.render(
+  <div>
+    <p> Created by Bickeykr </p>
+    <p> Copyright 2022 </p>
+  </div>,
+  document.getElementById("root")
+);
 //Created by YOURNAME.
 //Copyright CURRENTYEAR.
 //E.g.
